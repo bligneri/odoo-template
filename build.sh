bdcf413120e6b1ca3a0005bd13c3156a17ac48a6
Benoit des Ligneris <benoitdl@pm.me>
2023-03-03 21:41:08 -0500

    Local dev ignore and locally tested PATH_ODOO variable

--- build.sh
+++ build.sh
@@ -3,9 +3,23 @@
 # get the odoo version to clone
 ODOOVERSION=$(cat odoo_version)
 
+# When developing locally
+# Just `touch local_dev`
+# And you can test the build script locally (odoo will be in ./odoo
+# On Platform.sh containers, odoo will be on /app/odoo/
+
+FILE=local_dev
+if [ -f "$FILE" ]; then
+    PATH_ODOO="."
+else
+    PATH_ODOO="/app"
+fi
+
+PATH_ODOO="${PATH_ODOO}/odoo"
+
 #clone the repo
-git clone https://www.github.com/odoo/odoo --depth 1 --branch $ODOOVERSION /app/odoo/
+git clone https://www.github.com/odoo/odoo --depth 1 --branch $ODOOVERSION $PATH_ODOO
 
 # Install dependencies
-cd /app/odoo/odoo
+cd $PATH_ODOO
 pip install ./

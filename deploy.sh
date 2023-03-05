ef2a595fe9af1dce04f422862fa9a1a212da7507
Benoit des Ligneris <benoitdl@pm.me>
2023-03-05 11:14:27 -0500

    all tree rsync

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@ APP_DIR=`pwd`
 
 # Copy the add_ons in their new home
 # Clean the addons directory and copy from the source
-rm -rf "$APP_DIR/odoo/addons" && cp -r "$APP_DIR/odoo-src/odoo/addons" "$APP_DIR/odoo/"
-ADDON_PATH=$APP_DIR/odoo/addons
+# Rsync the whole tree
+rsync -av ./odoo-src ./odoo
+#rm -rf "$APP_DIR/odoo/addons" && cp -r "$APP_DIR/odoo-src/" "$APP_DIR/odoo/"
+#ADDON_PATH=$APP_DIR/odoo/addons
 
 # Recover the plaftorm.sh variables required to install odoo 
 DB_HOST=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].host"`
@@ -14,5 +16,5 @@ DB_NAME=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].pa
 
 # Initialize the server and stop
 # We want to use WSGI to access it
-cd $APP_DIR/odoo-src/
+cd ./odoo/
 ./odoo-bin --init --addons-path=$ADDON_PATH --db_host=$DB_HOST --db_user=$DB_USER --db_password="$DB_PASSWORD" -d $DB_NAME --stop-after-init

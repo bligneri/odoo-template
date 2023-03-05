b03981b08cac565e221c207f4288f9aacec09f7f
Benoit des Ligneris <benoitdl@pm.me>
2023-03-05 10:04:59 -0500

    using directly the addon directory on the read-only partition

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Copy the add_ons in their new home
 # Clean the addons directory and copy from the source
-rm -rf  odoo/addons && cp -r odoo-src/odoo/addons odoo/
+#rm -rf  odoo/addons && cp -r odoo-src/odoo/addons odoo/
 
 # Recover the plaftorm.sh variables required to install odoo 
 DB_HOST=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].host"`
@@ -12,4 +12,4 @@ DB_NAME=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].pa
 
 # Initialize the server and stop
 # We want to use WSGI to access it
-odoo --init --addons-path=/app/odoo/addons --db_host=$DB_HOST --db_user=$DB_USER --db_password="$DB_PASSWORD" -d $DB_NAME --stop-after-init
+odoo --init --addons-path=odoo-src/addons --db_host=$DB_HOST --db_user=$DB_USER --db_password="$DB_PASSWORD" -d $DB_NAME --stop-after-init

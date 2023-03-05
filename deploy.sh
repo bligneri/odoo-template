4ece023272cb2903413e6d7038c7429c75427357
Benoit des Ligneris <benoitdl@pm.me>
2023-03-05 09:44:32 -0500

    Explicit path for the addons

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Copy the add_ons in their new home
+cp -r /tmp/odoo/odoo/addons /app/odoo/
+
+# Recover the plaftorm.sh variables required to install odoo 
 DB_HOST=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].host"`
 DB_USER=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].username"`
 DB_PASSWORD=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].password"`
@@ -7,4 +11,4 @@ DB_NAME=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].pa
 
 # Initialize the server and stop
 # We want to use WSGI to access it
-odoo --init --addons-path=odoo/addons --db_host=$DB_HOST --db_user=$DB_USER --db_password="$DB_PASSWORD" -d $DB_NAME --stop-after-init
+odoo --init --addons-path=/app/odoo/addons --db_host=$DB_HOST --db_user=$DB_USER --db_password="$DB_PASSWORD" -d $DB_NAME --stop-after-init

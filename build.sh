af7efd0574786e7cfdd1f39c587a8400dbe5da81
Benoit des Ligneris <benoitdl@pm.me>
2023-03-04 21:13:27 -0500

    Python 3.11 and pip update

--- build.sh
+++ build.sh
@@ -5,13 +5,16 @@ ODOOVERSION=$(cat odoo_version)
 
 PATH_ODOO="/tmp/"
 
+# Update to the latest pip
+python3.11 -m pip install --upgrade pip
+
 # DEBUG
 #Delete the current ./odoo
 #rm -rf ./odoo
 # END DEBUG
 
 #clone the repo
-git clone https://www.github.com/odoo/odoo --depth 1 --branch $ODOOVERSION $PATH_ODOO
+git clone https://www.github.com/odoo/odoo --depth 1 --branch $ODOOVERSION $PATH_ODOO/odoo
 
 # Install dependencies
 cd $PATH_ODOO/odoo

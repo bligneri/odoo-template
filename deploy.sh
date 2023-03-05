32b82c5db3faa20f22bd164b9008ffee2b70e2c3
Benoit des Ligneris <benoitdl@pm.me>
2023-03-04 21:43:21 -0500

    Adding back the deploy script with env variable data

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-# what was there?
-cat /tmp/files
+DB_HOST=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].host"`
+DB_USER=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].username"`
+DB_PASSWORD=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].username"`
+DB_NAME="odoo"
 
-# Can we copy the content of the build to /app/odoo ?
-cp -r /tmp/odoo /app/odoo
-
-# can we copy a file?
-cp /tmp/files /app/dir_content.txt
-
-# Can we launch odoo?
-cd /app/odoo/odoo
-./odoo-bin
+# Initialize the server and start
+odoo --init --addons-path=odoo/addons --db_host=$DB_HOST --db_user=$DB_USER --db_password="$DB_PASSWORD" -d $DB_NAME

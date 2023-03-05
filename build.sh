dfb7f92aecf6fbfe9cf1f559bec1daa16c1535c4
Benoit des Ligneris <benoitdl@pm.me>
2023-03-05 09:27:58 -0500

    Trying to copy the addon directory

--- build.sh
+++ build.sh
@@ -3,23 +3,22 @@
 # get the odoo version to clone
 ODOOVERSION=$(cat odoo_version)
 
-PATH_ODOO="/tmp/"
+PATH_ODOO="/tmp"
 
 # Update to the latest pip
 python3.11 -m pip install --upgrade pip
 
-# DEBUG
-#Delete the current ./odoo
-#rm -rf ./odoo
-# END DEBUG
+# Note: the upgrade works but the updated version of pip is not used
+# => most cerainly a PATH issue
 
 #clone the repo
+# Note: Should be smarter and only download the code if not there for the $ODOOVERSION
+# If the code is already thre, no need to change it
 git clone https://www.github.com/odoo/odoo --depth 1 --branch $ODOOVERSION $PATH_ODOO/odoo
 
 # Install dependencies
 cd $PATH_ODOO/odoo
 pip install ./
 
-# Debug our build process: is there something in /tmp ?
-echo `ls -la /tmp/odoo` > /tmp/files
-echo `ls -la /tmp/odoo/odoo` >> /tmp/files
+# We now need to copy the add-ons to a directory that will persist after the build
+cp -r $PATH_ODOO/odoo/odoo/addons /app/odoo/addons

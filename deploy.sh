32cd9b1bc971e24d5b5692a8aefdd9562792a2d7
Benoit des Ligneris <benoitdl@pm.me>
2023-03-04 21:55:19 -0500

    Fixing dbname issue

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 DB_HOST=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].host"`
 DB_USER=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].username"`
-DB_PASSWORD=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].username"`
-DB_NAME="odoo"
+DB_PASSWORD=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].password"`
+DB_NAME=`echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq -r ".database[0].path"`
 
 # Initialize the server and start
 odoo --init --addons-path=odoo/addons --db_host=$DB_HOST --db_user=$DB_USER --db_password="$DB_PASSWORD" -d $DB_NAME

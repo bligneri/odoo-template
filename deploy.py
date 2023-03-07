f15dfab4cd146bde14b892409703657f61115b5d
Benoit des Ligneris <benoitdl@pm.me>
2023-03-06 21:45:38 -0500

    Creating a DEPLOYED file to avoid redoing the odoo intialization

--- deploy.py
+++ deploy.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 import os
 import sys
+import datetime
 from platformshconfig import Config
 import subprocess
 
-DIRECTORY = os.path.join(os.environ["HOME"], ".local", "share", "Odoo")
+TIMESTAMPFILE = os.path.join(os.environ["HOME"], ".local", "DEPLOYED")
 
+print(f"Testing for {TIMESTAMPFILE}")
 # If Odoo is already configured, end the deploy script.
-# if not os.path.isdir(DIRECTORY):
-#     print(f"{DIRECTORY} is not a directory\n")
-#     sys.exit(0)
+if os.path.isfile(TIMESTAMPFILE):
+    print(f"{TIMESTAMPFILE} exists: Odoo is installed.")
+    print(f"To reinstall, delete this file from the odoo mount.\n")
+    print(f"platform ssh and then rm {TIMESTAMPFILE}")
+    sys.exit(0)
 
 # Create a new Config object to ease reading the Platform.sh environment variables.
 # You can alternatively use os.environ yourself.
@@ -34,4 +38,18 @@ os.chdir(ODOO_DIR)
 print(f"Executing: {command}\n")
 process = subprocess.Popen(command, shell=True)
 process.wait()  # Wait for the process to complete
+
+##  SUCCESS
+## Let's write a file for initialization with a timestamp
+
+# Get current date and time
+now = datetime.datetime.now()
+
+# Format the date and time as a string
+timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
+# Create the file and write the message with timestamp
+with open(TIMESTAMPFILE, 'w') as file:
+    file.write("Deployed on " + timestamp)
+
 
